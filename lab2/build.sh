1c5f39563dd343e0eebb5d04be5a12d4402a68e3
Ardelean Raul <153224708+RaulArdelean18@users.noreply.github.com>
2026-04-04 13:55:37 +0300

    Build and .exe are now in Lab2 Directory

---
+++ lab2/build.sh
@@ -0,0 +1,6 @@
+g++ -std=c++20 -Wall -Wextra -Wshadow -Wconversion -pedantic -pipe \
+"Cerinta 39/Proiect C++/App.cpp" \
+"Cerinta 39/Proiect C++/EvaluareExpresie.cpp" \
+"TAD Coada/Proiect C++/Coada.cpp" \
+"TAD Stiva/Proiect C++/Stiva.cpp" \
+-o app
